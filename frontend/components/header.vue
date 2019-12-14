a688053f03959ca21e3f8a7507bc7d953261da33
yoshimi_n <yoshimi_n@usernoMacBook-puro-4.local>
2019-12-14 16:11:41 +0900

    element-uiの導入

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -1,6 +1,23 @@
 <template>
-  <el-menu class="el-memu-demo" mode="horizontal">
-    <el-menu-item index="1">こんにちは</el-menu-item>
-    <el-menu-item index="2">コンバンワ</el-menu-item>
+  <el-menu :default-active="activeIndex" class="el-memu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1">Reacher</el-menu-item>
+    <el-menu-item index="2">目標設定</el-menu-item>
+    <el-menu-item index="3">学習管理</el-menu-item>
+    <el-menu-item index="4">ToDo</el-menu-item>
   </el-menu>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
