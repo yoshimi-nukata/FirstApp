0fa303ccff440a735fd3119cabea4fc2f3180324
yoshimi_n <yoshimi_n@usernoMacBook-puro-4.local>
2019-12-14 14:19:42 +0900

    header,footer作成

---
+++ frontend/components/header.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-menu class="el-memu-demo" mode="horizontal">
+    <el-menu-item index="1">こんにちは</el-menu-item>
+    <el-menu-item index="2">コンバンワ</el-menu-item>
+  </el-menu>
+</template>
